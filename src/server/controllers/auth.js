52b648864784125f3f28ed9753ba9f46e719881d
Mudpadans <philmodel.101@gmail.com>
2023-08-16 18:35:25 -0400

    port

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -1,7 +1,26 @@
+require('dotenv').config()
+const SECRET = process.env.SECRET
+const jwt = require('jsonwebtoken')
+
+const createToken = (username, id) => {
+    return jwt.sign(
+        {
+            username,
+            id
+        },
+        SECRET,
+        {
+            expiresIn: '2 days'
+        }
+    )
+}
+
 module.exports = {
     login: async (req, res) => {
+        let {username, password} = req.body
+        const token = createToken(username, password)
         console.log('login') 
-        res.sendStatus(200)
+        res.status(200).send(token)
     },
     register: async (req, res) => {
         console.log('register')
