2c3277503283e27b4e7544cba5423de731d6e14d
Mudpadans <philmodel.101@gmail.com>
2023-08-23 00:10:40 -0400

    env problems

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,18 @@
+require('dotenv').config();
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = process.env.PORT || 4004
+const { PORT } = process.env || 4004
+const {sequelize} = require('./util/database')
+const {User} = require('./models/user')
+const {Post} = require('./models/post')
 
 app.use(express.json())
 app.use(cors())
 
 const {getAllPosts, getCurrentUserPosts, addPost, editPost, deletePost} = require('./controllers/posts')
 const {register, login} = require('./controllers/auth')
+const {isAuthenticated} = require("./middleware/IsAuthenticated")
 
 app.post('/register', register)
 app.post('/login', login)
@@ -15,8 +20,17 @@ app.post('/login', login)
 app.get('/posts', getAllPosts)
 
 app.get('./userposts/:userId', getCurrentUserPosts)
-app.post('/posts', addPost)
-app.put('/posts/:id', editPost)
-app.delete('/posts/:id', deletePost)
+app.post('/posts', isAuthenticated, addPost)
+app.put('/posts/:id', isAuthenticated, editPost)
+app.delete('/posts/:id', isAuthenticated, deletePost)
 
-app.listen(PORT, () => console.log(`Running on Port ${PORT}`))
+User.hasMany(Post)
+Post.belongsTo(User)
+
+sequelize.sync()
+.then(() => {
+    app.listen(PORT, () => console.log(`db sync successful & server running on port ${PORT}`))
+})
+.catch(err => console.log(err))
+
+// app.listen(PORT, () => console.log(`Running on Port ${PORT}`))
